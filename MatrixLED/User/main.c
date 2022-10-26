25f16336ef3df0b328d2549d333fa457afb618d5
加多小王子 <kingduoduo@hotmail.com>
2022-10-26 09:36:31 +0800

    删除不要文件

---
+++ MatrixLED/User/main.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: king jing
+ * @Date: 2022-10-21 15:11:07
+ * @LastEditTime: 2022-10-21 15:24:26
+ * @Description: 
+ */
+
+
+#include "Public.h"
+
+void main(void)
+{
+
+	while (1)
+	{
+
+
+	}
+}
--- bug_keys_led/Bsp/i2c/i2c.c
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: king jing
- * @Date: 2022-10-18 16:36:06
- * @LastEditTime: 2022-10-18 17:14:58
- * @Description: Do not edit
- */
-#include "i2c.h"
