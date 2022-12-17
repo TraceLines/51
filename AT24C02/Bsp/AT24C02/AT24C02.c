b161dfb626b80b682bf55603895cb01cd9e84c35
kingduoduo <kingduoduo@Hotmail.com>
2022-12-17 11:57:49 +0800

    add

--- AT24C02/Bsp/AT24C02/AT24C02.c
+++ AT24C02/Bsp/AT24C02/AT24C02.c
@@ -5,7 +5,7 @@
 #define AT24C02_ADDRESS 0XA0
 
 /**
- * @brief 向从机写数据
+ * @brief 向从机写数据，字节写，整个过程由AT24C02的写时序决定的
  * @param WordAddress 被写的地址
  * @param ByteData    被写的数据
  *
@@ -33,7 +33,7 @@ void AT24C02_WriteByte(unsigned char WordAddress, ByteData)
 }
 
 /**
- * @brief 从slave中读取数据
+ * @brief 从slave中读取数据，字节读，整个过程由AT24C02的读时序决定的
  * @param WordAddress 要读的地址
  * @return ByteData 读到的数据
  */
@@ -54,7 +54,7 @@ unsigned char AT24C02_ReadByte(unsigned char WordAddress)
     I2C_SendByte(AT24C02_ADDRESS | 0x01);
     I2C_ReceiveACK();
     ByteData = I2C_ReceiveByte();
-    // 接收完一位，作为接收者，发送一个ACK
+    // 接收完一位，作为主机，接收者，又准备结束通信的情况下，发送一个NACK，让从机释放SCL和SDA，以便作为主机的MCU结束通信
     I2C_SendACK(1);
     I2C_Stop();
     // -----------通信的流程 end
